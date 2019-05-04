995560df5579363af1b8a74b114c167b53e6dd62
ikeike443 <ikeike443+octodemo1@github.com>
2019-05-04 16:52:20 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SMAP</h1>
+          <h1>Simple SMAP!</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
